2d30e618982efa0f6d4284d3138b6e7ac92a9f8b
radamshiya <122056107+radamshiya@users.noreply.github.com>
2025-11-30 18:37:09 -0800

    adding github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yourusername.github.io',
-  // Uncomment for GitHub Pages deployment:
-  // base: '/repository-name',
+  site: 'https://radamshiya.github.io',
+  base: '/Ryan_portfolio',
 });
