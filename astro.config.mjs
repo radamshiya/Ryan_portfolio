a7056d9487ab01a73c2cdf1e6d96aea293ba3a3e
radamshiya <122056107+radamshiya@users.noreply.github.com>
2025-11-30 18:53:00 -0800

    more link updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://radamshiya.github.io',
-  base: '/Ryan_portfolio',
+  base: '/Ryan_portfolio/',
 });
